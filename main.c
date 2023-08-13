a832551e1a68964090a195594619dac9e6dfb993
desaiparimal <50705378+desaiparimal@users.noreply.github.com>
2023-08-13 10:16:24 +0100

    First Pass commit with multithreading Added pthread support First is main thread Second is pollstdin thread third is timer thread
    press ctrl+d to stop running program

--- main.c
+++ main.c
@@ -1,71 +1,102 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
 
-void timecalc(int minutes){
-    unsigned int x_seconds=0;
-	unsigned int x_milliseconds=0;
-	unsigned int count_down_time_in_secs=0,time_left=0;
-	clock_t x_startTime,x_countTime;
-	count_down_time_in_secs=minutes*60;  // 1 minute is 60secs, 1 hour is 3600
+int mRunning = 0;
 
- 
-    x_startTime=clock();  // start clock
-    time_left=count_down_time_in_secs-x_seconds;   // update timer
+struct stream_data{
+  ssize_t length;
+  char *string;
+  size_t buffer_size;
+};
 
-	while (time_left>0)	{
-		x_countTime=clock(); // update timer difference
-		x_milliseconds=x_countTime-x_startTime;
-		x_seconds=(x_milliseconds/(CLOCKS_PER_SEC));
-
-		time_left=count_down_time_in_secs-x_seconds; // subtract to get difference
-
-		//printf( "\nYou have %d seconds left ( %d ) count down timer ",time_left,
-        //          count_down_time_in_secs);
-	}
-
-
-	printf( "\n\n\nTime's out\n\n\n");
-
-}
-
-int main(){
-    char *str = NULL;
-    size_t len = 0;     /* ignored when line = NULL */
-    ssize_t read;
-    unsigned minuets = 0;
-
-    printf ("\nEnter minutes for heater timer \n");
-    scanf("%u",&minuets);
-    printf ("\nEnter schedule in a string below and enter ,press [ctrl + d] to quit\n");
-
-    while ((read = getline(&str, &len, stdin)) != -1) {
-
-        if (read > 0)
-            printf ("\n  read %zd chars from stdin, allocated %zd bytes for line : %s\n", read, len, str);
-
-        if(strlen(str)!=49){
+void* pollstdin (void* info)
+{
+    struct stream_data *stream = (struct stream_data *)malloc(sizeof(struct stream_data));
+    stream->string = NULL;
+    stream->length = 0;
+    stream->buffer_size = 0 ;
+    while((stream->length = getline(&(stream->string), &(stream->buffer_size), stdin)) != -1 ){
+        if (stream->length > 0)
+            printf ("\n  read %zd chars from stdin, allocated %zd bytes for line : %s\n",
+                     stream->length, stream->buffer_size, stream->string);
+        if(stream->length != 49){
             printf("Enter 48digits only\n");
             continue;
         } else{
-            for (int i = 0 ;i < strlen(str)-1;i++){
-                int status = str[i] - '0';
-                //printf("status = %d \n", status);
+            for (int i = 0 ;i < stream->length - 1 ; i++){
+                int status = stream->string[i] - '0';
                 if(!(status == 0 || status == 1)){
                     printf("Enter either 0 or 1 digits only \n");
                     continue;
                 }
             }
         }
-        for (int i = 0 ;i < strlen(str)-1;i++){
-            if(str[i] - '0')
-                printf("ON\n");
-            else    
-                printf("OFF\n");
-            timecalc(minuets);
+        mRunning = 1;
+        return (void*) stream;
+    }
+    return (void *)stream;
+}
+void *timer(void *vargp)
+{
+    struct stream_data *new = (struct stream_data *) vargp;
+    if(new == NULL)
+        pthread_exit(pthread_self());
+
+	unsigned int x_seconds=0;
+	unsigned int x_milliseconds=0;
+	unsigned int count_down_time_in_secs=0,time_left=0;
+	clock_t x_startTime,x_countTime;
+	count_down_time_in_secs=5;  // 1 minute is 60, 1 hour is 3600
+    mRunning = 0 ;
+    for (int i = 0 ; i < new->length - 1 ; i++ ){
+        x_seconds=0 ; 
+        if(new->string[i] - '0')
+            printf("ON\n");
+        else    
+            printf("OFF\n");
+
+        x_startTime=clock();  // start clock
+        time_left=count_down_time_in_secs-x_seconds;   // update timer
+
+        while (time_left > 0 && !mRunning) {
+            x_countTime=clock(); // update timer difference
+            x_milliseconds=x_countTime-x_startTime;
+            x_seconds=(x_milliseconds/(CLOCKS_PER_SEC));
+
+            time_left=count_down_time_in_secs-x_seconds; // subtract to get difference 
+            if(mRunning == 1)
+                pthread_exit(pthread_self());
+
         }
-        printf ("Enter string below [ctrl + d] to quit\n");
+        printf( "  Time's out\n");
+    }
+    return (void*) 0;
+}
+   
+int main(){
+    struct stream_data *new = NULL ;
+    //unsigned minutes = 0;
+
+    //printf ("\nEnter minutes for heater timer \n");
+    //scanf("%u",&minutes);
+    printf ("\nEnter schedule in a string below and enter ,press [ctrl + d] to quit\n");
+
+    pthread_t thread_id1,thread_id2;
+
+    while (1) {
+
+        pthread_create(&thread_id1, NULL, pollstdin, NULL);
+        pthread_join(thread_id1, (void **)&new);
+
+        if(new ==  NULL || new->length == -1){
+            pthread_exit(pthread_self()); // pressed ctrl+d
+            mRunning = 1; //stopping the timer thread
+            return 0;
+        }
+
+        pthread_create(&thread_id2, NULL, timer, (void *)new);
     }    
     return 0;
 }
